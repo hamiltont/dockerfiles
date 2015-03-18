54f2564ea57729f01832ff086ff5aa48fc0a830f
Hamilton Turner <hamiltont@gmail.com>
2015-03-17 22:22:25 -0400

    Minor updates

--- services/nzbget/start.sh
+++ services/nzbget/start.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-[ ! -f /nzbdata/nzbget.conf ] && cp /usr/local/share/nzbget/nzbget.conf /nzbdata/nzbget.conf
+[ ! -f /nzbget/nzbget.conf ] && cp /usr/local/share/nzbget/nzbget.conf /nzbget/nzbget.conf
 
 # Tried using `--option DaemonUsername=$MEDIA_USER` but it seems to be ignored
 # TODO - Once docker exec supports --user, change the Dockerfile to say USER
 # instead of using su to drop root. If someone somehow gets a console out of 
 # nzbget, I'd rather they not just say 'logout' and arrive at the root user ;)
-exec su -c "nzbget --configfile /nzbdata/nzbget.conf --daemon" - $MEDIA_USER
+exec su -c "nzbget --configfile /nzbget/nzbget.conf --daemon" - $MEDIA_USER
